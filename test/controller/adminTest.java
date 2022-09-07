e85488500b4ad716cb4ab10890d41e55580d042f
S. M. Fahim Faisal <63847173+FahimOni@users.noreply.github.com>
2022-09-07 17:26:39 +0600

    Add files via upload

---
+++ test/controller/adminTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Hasnayen
+ */
+public class adminTest {
+    
+    public adminTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of login method, of class admin.
+     */
+    @Test
+    public void testLogin() throws Exception {
+        System.out.println("login");
+        String email = "s.m.fahim.faisal@g.bracu.ac.bd";
+        String pass = "1234";
+        admin instance = new admin();
+        boolean expResult = true;
+        boolean result = instance.login(email, pass);  
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+    }
+    
+}
